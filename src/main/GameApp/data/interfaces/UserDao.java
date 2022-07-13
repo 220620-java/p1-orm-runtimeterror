7e5699ffd9c07edc41943541a59541aa3ae6931b
Hannah Bush <heb.rutgers@gmail.com>
2022-07-13 15:38:38 -0400

    restructuring

--- src/main/GameApp/data/interfaces/UserDao.java
+++
@@ -1,9 +0,0 @@
-package main.BankApp.data.interfaces;
-
-import main.BankApp.models.User;
-
-public interface UserDao {
-    public User findByUsername(String username);
-    public User create(User user);
-    public User getUserInfo(User user);
-}
